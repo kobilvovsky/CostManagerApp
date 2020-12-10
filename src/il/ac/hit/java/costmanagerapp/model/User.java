f02f3991ab3c3abaffa07ed0ec781312d280a114
nati <nati1143@gmail.com>
2020-12-10 15:52:48 +0200

    userClass

---
+++ src/il/ac/hit/java/costmanagerapp/model/User.java
@@ -0,0 +1,43 @@
+package il.ac.hit.java.costmanagerapp.model;
+
+public class User {
+    private int id;
+    private String user;
+    private String password;
+
+
+    public User(int id, String user, String password) {
+        setId(id);
+        setPassword(password);
+        setUser(user);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        int size=Integer.toString(id).length();
+        if(size==9) {
+            this.id = id;
+        }
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        if (password.length()==8) {
+            this.password = password;
+        }
+    }
+}
